0d090054ba4603d01d8ef778d8140bb3011390d2
konung-yaropolk <yaropolk1995@gmail.com>
2024-05-02 19:44:38 -0500

    modified:   dev/ui2py.bat 	modified:   dev/ui2py.sh 	modified:   spectroscope/ui_spectroscope.py 	modified:   spectroscope/ui_spectroscope_baseline.py 	modified:   spectroscope/ui_spectroscope_colors.py 	modified:   spectroscope/ui_spectroscope_persistence.py 	modified:   spectroscope/ui_spectroscope_settings.py 	modified:   spectroscope/ui_spectroscope_settings_help.py 	modified:   spectroscope/ui_spectroscope_smoothing.py 	modified:   spectroscope/version.py 	new file:   ui_spectroscope.py 	new file:   ui_spectroscope_baseline.py 	new file:   ui_spectroscope_colors.py 	new file:   ui_spectroscope_persistence.py 	new file:   ui_spectroscope_settings.py 	new file:   ui_spectroscope_settings_help.py 	new file:   ui_spectroscope_smoothing.py

--- dev/ui2py.sh
+++ dev/ui2py.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-cd ../spectroscope/ || exit
+pyuic6 ../spectroscope/spectroscope.ui -o ../spectroscope/ui_spectroscope.py
+pyuic6 ../spectroscope/spectroscope_baseline.ui -o ../spectroscope/ui_spectroscope_baseline.py
+pyuic6 ../spectroscope/spectroscope_colors.ui -o ../spectroscope/ui_spectroscope_colors.py
+pyuic6 ../spectroscope/spectroscope_persistence.ui -o ../spectroscope/ui_spectroscope_persistence.py
+pyuic6 ../spectroscope/spectroscope_settings_help.ui -o ../spectroscope/ui_spectroscope_settings_help.py
+pyuic6 ../spectroscope/spectroscope_settings.ui -o ../spectroscope/ui_spectroscope_settings.py
+pyuic6 ../spectroscope/spectroscope_smoothing.ui -o ../spectroscope/ui_spectroscope_smoothing.py
 
-pyuic6 spectroscope.ui -o ui_spectroscope.py
-pyuic6 spectroscope_baseline.ui -o ui_spectroscope_baseline.py
-pyuic6 spectroscope_colors.ui -o ui_spectroscope_colors.py
-pyuic6 spectroscope_persistence.ui -o ui_spectroscope_persistence.py
-pyuic6 spectroscope_settings_help.ui -o ui_spectroscope_settings_help.py
-pyuic6 spectroscope_settings.ui -o ui_spectroscope_settings.py
-pyuic6 spectroscope_smoothing.ui -o ui_spectroscope_smoothing.py
-
+echo "Done!"
 echo "Do anykey to exit..."
 read
